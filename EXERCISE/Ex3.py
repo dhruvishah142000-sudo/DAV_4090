f336bea464e1ece91d96c9bd8399e32765ac1c62
dhruvishah142000-sudo <dhruvi.shah142000@marwadiuniversity.ac.in>
2026-01-27 14:26:42 +0530

    Update Ex3.py

--- EXERCISE/Ex3.py
+++ EXERCISE/Ex3.py
@@ -1,12 +1,17 @@
 # Num is pos.,neg.,zero...
 
-n1 = int(input("Enter Number : "))
+try:
+    n1 = int(input("Enter Number : "))
 
-if n1>=1:
-    print(f"{n1} is Positive Number.")
+    if n1>=1:
+        print(f"{n1} is Positive Number.")
 
-elif n1<0:
-    print(f"{n1} is Negative Number.")
+    elif n1<0:
+        print(f"{n1} is Negative Number.")
 
-else:
-    print(f"Number is {n1}.")
+    else:
+        print(f"Number is {n1}.")
+
+except ValueError:
+    print("Wrong Input...")
+
