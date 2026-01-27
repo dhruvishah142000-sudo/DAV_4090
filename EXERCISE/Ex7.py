af1ded47675508772f477c6f340d41300bb26a1f
dhruvishah142000-sudo <dhruvi.shah142000@marwadiuniversity.ac.in>
2026-01-27 13:59:58 +0530

    Update Ex7.py

--- EXERCISE/Ex7.py
+++ EXERCISE/Ex7.py
@@ -1,7 +1,8 @@
 # Convert Distance Km to miles
 
-km = int(input("\n Enter Kilo Meter : "))
+km = float(input("\n Enter Kilo Meter : "))
 
 miles = km * 0.62
 
 print(f"\n {km} km Distance in miles is - {miles} miles. ")
+
