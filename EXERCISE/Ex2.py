4101693c378f127b26b1d3a946367bd0f064ad6e
dhruvishah142000-sudo <dhruvi.shah142000@marwadiuniversity.ac.in>
2026-01-27 13:54:58 +0530

    Update Ex2.py

--- EXERCISE/Ex2.py
+++ EXERCISE/Ex2.py
@@ -1,8 +1,9 @@
 # Simple Interest
 
 p = int(input("Enter Principle amount : "))
-r = int(input("Enter Rate : "))
+r = float(input("Enter Rate : "))
 n = int(input("Enter No. of years : "))
 
 si = (p*r*n)/100
 print(f"Principle Amount - {p} , Rate - {r} , For no. of year - {n} year \n Simple Interest is : {si}")
+
