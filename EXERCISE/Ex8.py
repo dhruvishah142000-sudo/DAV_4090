65e77eb69832c679bcaac268be56deed9be48887
dhruvishah142000-sudo <dhruvi.shah142000@marwadiuniversity.ac.in>
2026-01-27 14:04:54 +0530

    Update Ex8.py

--- EXERCISE/Ex8.py
+++ EXERCISE/Ex8.py
@@ -1,8 +1,9 @@
 # Convert Temparature from celcious to ferehit
 
-c = int(input("\n Enter Celcious : "))
+c = float(input("\n Enter Celcious : "))
 
 ferenhit = (c * 1.8 + 32)
 
 print(f"\n Celcious of Temparature : {c} ")
 print(f"\n Ferenhit of Temparature is : {ferenhit}.")
+
